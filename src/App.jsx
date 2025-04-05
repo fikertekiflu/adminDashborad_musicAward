b252b312474eba588148f6c50a3d5a0b1c20d4bd
fikertekiflu <casssiopfiker@gmail.com>
2025-04-05 20:56:16 +0300

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
         <Route path="/dashboard" element={<MainDashboard />} />
       </Routes>
     </Router>
